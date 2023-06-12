a441ca6d15043655e30916cae6cebad937179c4f
med <moarabat131@gmail.com>
2023-06-12 22:10:18 +0200

    first commit

---
+++ admin_area/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>NR-Store Admin Area</title>
+    <link rel="stylesheet" href="css/style/styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/style/styles/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style/styles/style.css">
+</head>
